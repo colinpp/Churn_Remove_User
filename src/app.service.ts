e3d2924ccb4bb20e5fcf05ea8d55f1a338eaf2b0
colinpp <colinpp1@gmail.com>
2022-11-14 14:10:21 +0200

    Changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { getToken, getUserJid, deleteUser } from './axios';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Ayoba!';
+  async deleteUser(churnObj: any): Promise<string> {
+    let access_token ='';
+    let userJid = '';
+    let outputStr = 'Saved Churn Data';
+    if (churnObj.port_in === 'false' && churnObj.port_out === 'false') {
+      access_token = await getToken();
+      userJid = await getUserJid(access_token, churnObj.msisdn);
+      outputStr =
+        'Saved churn Data And Deleted user ith msisdn of ' + churnObj.msisdn;
+    }
+
+    return outputStr;
   }
 }
